76b5e321bb8dfda171781a5a8b1e32e54fe04a83
JILSE7 <saidnnnn@gmail.com>
2022-08-15 22:45:53 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vite-kkuu5c
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vite-kkuu5c)
---
+++ __test__/hola.test.ts
@@ -0,0 +1,13 @@
+
+
+
+  test('should upload a file succesfull', async() => { 
+      const imageURL = "https://res.cloudinary.com/dxqnlqxa1/image/upload/v1629583063/zpa8fuxbdj5cgyj3gj0r.jpg";
+      const blob = await (await fetch(imageURL)).blob()
+      console.log(blob);
+      const file = new File([blob], "my-image");
+      // const url = await fileUpload(file);
+
+      // expect(url).toBe(true);
+
+  });
